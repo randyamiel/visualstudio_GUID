355cfb2a47c8f4b4d7040c6e65b9f5a550f54555
Randy Amiel <randyamiel@users.noreply.github.com>
2017-03-21 11:48:43 -0400

    Add files via upload

---
+++ SeqGuid/SequentialGuid.Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Randy Amiel 2017">
+//     Copyright © Randy Amiel 2017
+// </copyright>
+//-----------------------------------------------------------------------
+namespace SequentialGuid.Demo
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Windows.Forms;
+
+  /// <summary>
+  /// The entry point of the application.
+  /// </summary>
+  public static class Program 
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    public static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new SequentialGuidDemoForm());
+    }
+  }
+}
